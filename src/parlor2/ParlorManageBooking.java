0400e5963e04b3c9100395b18b74eeffe6db893e
Dell <amritaganguly111@gmail.com>
2019-04-19 05:11:17 +0600

    Manage booking( Customer ) done

---
+++ src/parlor2/ParlorManageBooking.java
@@ -0,0 +1,57 @@
+package parlor2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dell
+ */
+public class ParlorManageBooking
+{
+    
+private int bookingID;
+private String ParlorName;
+private String ServiceName;
+private String Price;
+private String DateAndTime;
+private String BookStatus;
+private String BStatus;
+        public ParlorManageBooking(int bookingID,String ParlorName,String ServiceName,String Price,String DateAndTime,String BookStatus)
+        {
+            this.ParlorName=ParlorName;
+            this.ServiceName=ServiceName;
+            this.Price=Price;
+            this.DateAndTime=DateAndTime;
+            this.BookStatus=BookStatus;
+            this.bookingID=bookingID;
+        }
+        public int getBookingId()
+        {
+            return bookingID;
+        }
+        public String getParlorName()
+        {
+            return  ParlorName;
+        }
+        public String getServiceName()
+        {
+            return ServiceName;
+        }
+        public String getPrice()
+        {
+            return Price;
+        }
+        public String getDateandTime()
+        {
+            return DateAndTime;
+        }
+        public String getBookStatus()
+        {
+            return BookStatus;
+        }
+    
+}
