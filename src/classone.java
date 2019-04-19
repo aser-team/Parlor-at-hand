329997cccf2656c15bcd9cc72e952f5129850924
Aspire <anan.dhaka@gmail.com>
2019-04-19 23:36:43 +0600

    1st commit

---
+++ src/classone.java
@@ -0,0 +1,38 @@
+
+public class classone {
+ 
+    
+  
+  public String pname;
+    public String pfor;
+      public String pservicel;
+        public String pservicet;
+            public String powner;
+              public int pcontact;
+               public String pemail;
+                  public String username;
+                     public String password;
+                     private byte[] picture;
+  
+
+    
+    public classone(String pname, String pfor,String pservicel,String pservicet, String powner, int pcontact, String pemail,String username,String password, byte[] image)
+    {
+  this.pname =pname;
+  this.pfor=pfor;
+  this.pservicel=pservicel;
+  this.pservicet= pservicet;
+      this.powner=powner;
+          this.pcontact=pcontact;
+          this.pemail=pemail;
+          this.username= username;
+          this.password=password;
+          this.picture=image;
+    }
+    
+    
+ 
+    
+   
+    
+}
