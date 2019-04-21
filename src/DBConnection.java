d9cca1614c3aba298f20137e36d61eb0fff8b304
Aspire <anan.dhaka@gmail.com>
2019-04-21 22:10:49 +0600

    thr commit

---
+++ src/DBConnection.java
@@ -0,0 +1,35 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aspire
+ */
+public class DBConnection {
+   public Connection getConnection(){
+    Connection con=null;
+    try 
+    {
+        con=DriverManager.getConnection("jdbc:mysql://localhost:3306/parlour","root","");
+        JOptionPane.showMessageDialog(null,"Connected");
+        return con;
+    } 
+    catch (SQLException ex) 
+    {
+        Logger.getLogger(classone.class.getName()).log(Level.SEVERE, null, ex);
+        JOptionPane.showMessageDialog(null,"Not Connected");
+        return null;
+    }
+    }
+}
