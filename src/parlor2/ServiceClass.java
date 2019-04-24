837e64f3942a4bc8bc03ceef9d660cb327540c17
Dell <amritaganguly111@gmail.com>
2019-04-24 22:14:46 +0600

    Service Table show items done.

---
+++ src/parlor2/ServiceClass.java
@@ -0,0 +1,47 @@
+package parlor2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dell
+ */
+public class ServiceClass {
+       
+private int pid;
+private String sname;
+private int sid;
+private String sprice;
+private String sdescription;
+        public ServiceClass(int pid,String sname,int sid,String sprice,String sdescription)
+        {
+            this.sname=sname;
+            this.sprice=sprice;
+            this.sdescription=sdescription;
+            this.pid=pid;
+            this.pid=pid;
+        }
+        public int getpid(){
+            return this.pid;
+        }
+        public String getsname()
+        {
+            return this.sname;
+        }
+          public int getsid()
+        {
+            return this.sid;
+        }
+        public String getsprice()
+        {
+            return this.sprice;
+        }
+        public String getsdescription()
+        {
+            return this.sdescription;
+        }
+}
